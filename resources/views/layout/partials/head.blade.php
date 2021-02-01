6aa98edd780a107f56a1983cbf6b294b462b4d33
echobinod <echobinod@gmail.com>
2021-02-01 20:03:24 +0545

    initial commit

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Album example for Bootstrap</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
